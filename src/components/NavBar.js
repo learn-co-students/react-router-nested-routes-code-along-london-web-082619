39af486812e03a2bedad23a2cf7f5d8e0e937b2b
Abdullah Bajaman <ambajaman@gmail.com>
2020-02-22 03:15:09 +0000

    fixed 2 table in api and add react-router-dom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,41 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink 
-        style={{ marginRight: '10px' }} 
-        to="/movies"
-      >
+    <div
+      style={{
+        borderBottom: "2px solid black",
+        paddingBottom: "10px",
+        marginBottom: "12px"
+      }}>
+      <NavLink style={{ marginRight: "10px" }} to="/movies">
         Movies
+      </NavLink>
+      <NavLink
+        to="/"
+        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+        exact
+        /* add styling to Navlink */
+        style={link}
+        /* add prop for activeStyle */
+        activeStyle={{
+          background: "darkblue"
+        }}>
+        Home
       </NavLink>
     </div>
   );
-}
+};
+/* Add basic styling for NavLinks */
+const link = {
+  marginRight: "10px",
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white"
+};
 
 export default NavBar;
--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,15 +1,23 @@
 // .src/containers/MoviesPage.js
-import React from 'react';
-import { Route } from 'react-router-dom';
-import MoviesList from '../components/MoviesList';
-import MovieShow from '../components/MovieShow';
+import React from "react";
+import { Route } from "react-router-dom";
+import MoviesList from "../components/MoviesList";
+import MovieShow from "../components/MovieShow";
 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
-    <Route exact path={match.url} render={() => <h1>Choose a movie from the list above</h1>} />
-    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> } />
+    <Route
+      exact
+      path={match.url}
+      render={() => <h1>Choose a movie from the list above</h1>}
+    />
+    <Route
+      path={`${match.url}/:movieId`}
+      render={(routerProps) => <MovieShow {...routerProps} movies={movies} />}
+    />
   </div>
-)
+);
 
-export default MoviesPage
+export default MoviesPage;
+
